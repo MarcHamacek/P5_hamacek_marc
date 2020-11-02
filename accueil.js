200eb19fcc9472004ca54722fe22ba1dab538ea9
MarcHamacek <62935905+MarcHamacek@users.noreply.github.com>
2020-11-02 14:52:28 +0100

    Accueil - Présentation dynamique

---
+++ accueil.js
@@ -0,0 +1,24 @@
+fetch("http://localhost:3000/api/cameras").then(function(response) {
+    return response.json();
+}) .then(function(data) {
+    const products = document.getElementById('products');
+    for(product of data) {
+        console.log(product)
+        products.innerHTML += ` 
+        <section class="col-12 col-lg-6">
+            <section class="card">
+                <img class="card-img-top" src="${product.imageUrl}" alt="">
+                <section class="card-body">
+                    <h2 class="card-title">${product.name}</h2>
+                    <p class="card-text">${product.description}</p>
+                </section>
+                <section class="btn-article">
+                    <a id="lien-produit-1" class="btn btn-success col-6 col-lg-4" href="produit.html?id=${product._id}" role="button">Voir détails</a>
+                </section>
+            </section>
+        </section>
+
+        `
+    }
+})
+
